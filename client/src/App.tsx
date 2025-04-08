95794a7fbef756dac48ba49743016373c17f66c5
Aly Zain <alyzain@Alys-MacBook-Air.local>
2025-04-08 16:57:12 +0200

    Set up Vite with React and TypeScript template, installed dependencies, and initialized project structure

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Welcome to the Full-Stack Authentication App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
